85a8c5ed45a32ec4247ef6d9ec0b81ef2d601e7a
Mishita0823 <mishitatiwari27@gmail.com>
2026-05-10 21:44:50 +0530

    Final commit

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "http://localhost:5000";
-export const TOKEN_KEY = 'crm_token';
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";export const TOKEN_KEY = 'crm_token';
 export const USER_KEY = 'crm_user';
 
 export const COMPANY_TYPES = [
